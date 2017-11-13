cb4440292ab301c7ef6f31ae9446271bbe9d20b6
Erik <erik@Eriks-MacBook-Air.local>
2017-11-13 10:39:21 +0100

    fix

--- BinHeap.java
+++ BinHeap.java
@@ -15,14 +15,20 @@ public class BinHeap<E extends Comparable<? super E>>  implements PrioQueue<E>{
 
 
 
-	if(arrayRep.size()>0){
+	if(arrayRep.size()>1){
 	    int currentIndex=arrayRep.size()-1;
 	    boolean needForBubble=true;
-	    
+	    int parentIndex;
 	 
 	    while(needForBubble){
-		int parentIndex=(currentIndex-1)/2;
-		if(compare(arrayRep.get
+		parentIndex=(currentIndex-1)/2;
+		if(comp.compare(arrayRep.get(currentIndex),arrayRep.get(parentIndex))<0){
+		    swap(currentIndex,parentIndex);
+		    currentIndex=parentIndex;
+		}
+		else{
+		    needForBubble=false;
+		}
 	    }
 	}
     }
@@ -32,17 +38,66 @@ public class BinHeap<E extends Comparable<? super E>>  implements PrioQueue<E>{
 
 
 
-
-
     }
+    public E poll(){
+	//	E result=arrayRep.remove(0);
+	swap(0,arrayRep.size()-1);
+	E result =arrayRep.remove(arrayRep.size()-1);
+	int currentIndex=0;
+	int childIndex;
+	while(true){
+	    childIndex=2*currentIndex+1;
+	    if(compare(arrayRep.get(childIndex),arrayRep.get(childIndex+1))>0){
+		childIndex++;
+	    }
+	    if(compare(arrayRep.get(childIndex),arrayRep.get(currentIndex))<0){
+		swap(currentIndex,childIndex);
+		currentIndex=parentIndex;
+
+	    }else{
+		break;
+	    }
+
+	    
+
+
+
+	}
+	
+	
+    }
+    // @Overridde 
     public void remove(E e){
+	for(int i=0;
 	
-	
+    }
+    public void swap(int i,int j){
+	E temp=arrayRep.get(i);
+	arrayRep.set(i,arrayRep.get(j));
+	arrayRep.set(j,temp);
     }
 
     /*    public static void bubble(){
       int insertedIndex=arrayRep.size()-1;
       }*/
+    public Iterator<E> iterator() {
+	Iterator<E> it =new Iterator<E>(){
+	    private int currentIndex=0;
+	    @Override
+	    public boolean hasNext() {
+		return currentIndex<arrayRep.size-1;
+	    }
+	    @Override
+	    public E next(){
+		return arrayRep.get(currentIndex++);
+	    }
+	    @Override
+	    public void remove(){
+		throw new UnsupportedOperationExpection();
+	    }
+	};
+	return it;
+    }
 }
 	
 	
